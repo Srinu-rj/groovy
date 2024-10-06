fbe4e6a8ad7a4989a5ed538bbff7cf2ad86b4edb
Sreenivasa Raju <95528410+Srinu-rj@users.noreply.github.com>
2024-10-06 17:13:42 +0530

    Update DockerImagePush.groovy

--- vars/DockerImagePush.groovy
+++ vars/DockerImagePush.groovy
@@ -1,11 +1,29 @@
-def call(String project, String ImageTag, String hubUser){
-    withCredentials([usernamePassword(
-            credentialsId: "docker",
-            usernameVariable: "srinu641",
-            passwordVariable: "srinu1234"
-    )]) {
-        sh "docker login -u '$USER' -p '$PASS'"
-    }
-    sh "docker image push ${hubUser}/${project}:${ImageTag}"
-    sh "docker image push ${hubUser}/${project}:latest"   
+// def call(String project, String ImageTag, String hubUser){
+//     withCredentials([usernamePassword(
+//             credentialsId: "docker",
+//             usernameVariable: "srinu641",
+//             passwordVariable: "srinu1234"
+//     )]) {
+//         sh "docker login -u '$USER' -p '$PASS'"
+//     }
+//     sh "docker image push ${hubUser}/${project}:${ImageTag}"
+//     sh "docker image push ${hubUser}/${project}:latest"   
+// }
+
+def dockerBuildAndPushToHub(String project, String imageTag, String dockerHubUser) {
+    sh """
+    # Authenticate with Docker Hub
+    docker login -u ${dockerHubUser} -p ${DOCKER_HUB_PASSWORD}
+
+    # Build the Docker image
+    docker image build -t ${dockerHubUser}/${project}:${imageTag} .
+
+    # Tag the Docker image
+    docker tag ${dockerHubUser}/${project}:${imageTag} ${dockerHubUser}/${project}:latest
+
+    # Push the Docker image to Docker Hub
+    docker push ${dockerHubUser}/${project}:${imageTag}
+    docker push ${dockerHubUser}/${project}:latest
+    """
 }
+
