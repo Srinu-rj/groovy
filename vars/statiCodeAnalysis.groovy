b62fa67ce6e8c3834b7a2421d2efa91c69eed7c2
Sreenivasa Raju <95528410+Srinu-rj@users.noreply.github.com>
2024-10-06 14:02:41 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,5 +1,7 @@
 def call(){
    withSonarQubeEnv(credentialsId: 'sonar-api') {
-      sh 'mvn clean package sonar:sonar'
+      // sh 'mvn clean package sonar:sonar'
+       sh ''' $SCANNER_HOHE/bin/sonar-scanner -Dsonar.projectName=spring-application  -Dsonar.projectKey=spring-application \
+         -Dsonar.java.binaries=. '''
    }
 }
