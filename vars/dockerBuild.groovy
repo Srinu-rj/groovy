c880365d0de1216f8e557b794e9fa1a2a0916e98
Sreenivasa Raju <95528410+Srinu-rj@users.noreply.github.com>
2024-10-06 16:10:13 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,10 +1,11 @@
-// def call(String project, String ImageTag){
-// sh """
-//   docker build -t ${hubUser}/${project}
-//   docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-//   docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
-// """
-// }
-def call(){
-    sh "docker build -t spring-image ."
+def call(String project, String ImageTag, String hubUser){
+    
+    sh """
+     docker image build -t ${hubUser}/${project} . 
+     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+    """
 }
+// def call(){
+//     sh "docker build -t spring-image ."
+// }
