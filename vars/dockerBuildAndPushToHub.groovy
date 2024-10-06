fb49fcc505a691151736306317c63a37733808ab
Sreenivasa Raju <95528410+Srinu-rj@users.noreply.github.com>
2024-10-06 18:14:08 +0530

    Update dockerBuildAndPushToHub.groovy

--- vars/dockerBuildAndPushToHub.groovy
+++ vars/dockerBuildAndPushToHub.groovy
@@ -1,10 +1,10 @@
 def call(String project, String ImageTag, String hubUser){
 withCredentials([usernamePassword(
  credentialsId: "docker-credentials",
- usernameVariable: "dnsrinu143@gmail.com",
+ usernameVariable: "srinu641",
  passwordVariable: "srinu1234"
 )]) {
-        sh "docker login -u '$dnsrinu143@gmail.com' -p '$srinu1234'"
+        sh "docker login -u '$srinu641' -p '$srinu1234'"
     }
     sh "docker image push ${hubUser}/${project}:${ImageTag}"
     sh "docker image push ${hubUser}/${project}:latest"   
