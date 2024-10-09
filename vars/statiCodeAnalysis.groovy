3ebf7e7b2dd87f139d850ba86114857fa70e8155
Sreenivasa Raju <95528410+Srinu-rj@users.noreply.github.com>
2024-10-09 14:17:14 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,15 +1,15 @@
-def call(){
-   withSonarQubeEnv(credentialsId: 'sonar-token-docker', installationName: 'sonar-one') {
-       sh ''' $SCANNER_HOHE/bin/sonar-scanner -Dsonar.projectName=spring-boot-application  -Dsonar.projectKey=spring-boot-application \
-         -Dsonar.java.binaries=. '''
-   }
-}
-
 // def call(){
-//   withSonarQubeEnv(credentialsId: 'sonar-api', installationName: 'sonar') {
+//    withSonarQubeEnv(credentialsId: 'sonar-token-docker', installationName: 'sonar-one') {
 //        sh ''' $SCANNER_HOHE/bin/sonar-scanner -Dsonar.projectName=spring-boot-application  -Dsonar.projectKey=spring-boot-application \
 //          -Dsonar.java.binaries=. '''
 //    }
 // }
+
+def call(){
+  withSonarQubeEnv(credentialsId: 'sonar-api', installationName: 'sonar') {
+       sh ''' $SCANNER_HOHE/bin/sonar-scanner -Dsonar.projectName=spring-boot-application  -Dsonar.projectKey=spring-boot-application \
+         -Dsonar.java.binaries=. '''
+   }
+}
 
 
