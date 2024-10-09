7231d6b07e513279d58eb983da2c71f3daaeb6d0
Sreenivasa Raju <95528410+Srinu-rj@users.noreply.github.com>
2024-10-09 14:16:39 +0530

    Update mvnBuild.groovy

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,3 +1,13 @@
-def call(){
-    sh 'mvn clean install'
+// def call(){
+//     sh 'mvn clean install'
+// }
+
+def callMavenBuild() {
+    withMaven(maven: 'Maven_3.9.9') {
+        sh 'mvn clean install'
+    }
 }
+
+// withMaven(globalMavenSettingsConfig: '', jdk: 'jdk17', maven: 'maven3', mavenSettingsConfig: '', traceability: true) {
+//    sh 'mvn clean install'
+// }
