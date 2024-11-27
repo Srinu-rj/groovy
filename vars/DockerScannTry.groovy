f861640f2697f9a0225cbbf34044194e16e527a3
Sreenivasa Raju <95528410+Srinu-rj@users.noreply.github.com>
2024-11-27 20:10:34 +0530

    Update DockerScannTry.groovy

--- vars/DockerScannTry.groovy
+++ vars/DockerScannTry.groovy
@@ -4,4 +4,15 @@ def call(String project, String ImageTag, String hubUser){
      trivy image ${hubUser}/${project}:latest > scan.txt
      cat scan.txt
     """
+
+    sh '''
+        # Install Trivy if not already installed
+        if ! command -v trivy &> /dev/null; then
+        curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin
+        fi
+
+        # Scan the Docker image
+        trivy image --exit-code 1 --severity HIGH,CRITICAL 
+                    '''
+    
 }
