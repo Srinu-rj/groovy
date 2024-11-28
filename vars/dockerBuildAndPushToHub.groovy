09de87c4600d2f04a66703a9878372128abb55d5
Sreenivasa Raju <95528410+Srinu-rj@users.noreply.github.com>
2024-11-28 21:40:37 +0530

    Update dockerBuildAndPushToHub.groovy

--- vars/dockerBuildAndPushToHub.groovy
+++ vars/dockerBuildAndPushToHub.groovy
@@ -1,13 +1,20 @@
-def call(String project, String ImageTag, String hubUser){
-withDockerRegistry([usernamePassword(
- credentialsId: "docker-cred",
- usernameVariable: "srinu641",
- passwordVariable: "srinu1234"
-)]) {
-        sh "docker login -u '$srinu641' -p '$srinu1234'"
-    }
-    sh "docker image push ${hubUser}/${project}:${ImageTag}"
-    sh "docker image push ${hubUser}/${project}:latest"   
+// def call(String project, String ImageTag, String hubUser){
+// withDockerRegistry([usernamePassword(
+//  credentialsId: "docker-cred",
+//  usernameVariable: "srinu641",
+//  passwordVariable: "srinu1234"
+// )]) {
+//         sh "docker login -u '$srinu641' -p '$srinu1234'"
+//     }
+//     sh "docker image push ${hubUser}/${project}:${ImageTag}"
+//     sh "docker image push ${hubUser}/${project}:latest"   
+// }
+// }
+
+def call(){
+     withDockerRegistry(credentialsId: 'docker-cred', toolName: 'docker') {
+       sh "docker tag spring-image srinu641/spring-image:latest"
+       sh "docker push srinu641/spring-image:latest" 
 }
 
 // withDockerRegistry(credentialsId: 'docker-cred', toolName: 'docker', url: 'https://hub.docker.com/') {
